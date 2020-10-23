0090d29a62982768a9f7ee48910c0e8c9cdff3c6
amir hosein <a.h.keshavarz74@gmail.com>
2020-10-23 15:40:27 +0330

    creating icahedron and testing TODA @ 990801
    PROBLEM: TODA test have wrong answer.

---
+++ main.m
@@ -0,0 +1,90 @@
+clc
+clear
+close all
+
+%% Parameteres
+faceopaque = false;
+icahedron_plot = true;
+
+c = 1; % m/s speed of sound
+
+L = 1;
+nPoints = 10*4^L + 2;
+
+test_point = rand(1, 3)-0.5;
+test_point = test_point / norm(test_point);
+%% microphones location
+theta = linspace(0, 2*pi, 9).';
+mics_locs = 0.254/2*[cos(theta), sin(theta), zeros(length(theta), 1)];
+mics_locs = mics_locs(1:end-1, :);
+
+%% creating test points
+[vMat, fMat] = spheretri(nPoints);
+
+%% plot
+if icahedron_plot == true
+    x = vMat(:, 1);
+    y = vMat(:, 2);
+    z = vMat(:, 3);
+    if faceopaque == true
+        h=trisurf(fMat,x,y,z, 'FaceColor', 'white', 'EdgeColor', 0.2*[1 1 1], 'LineWidth', 1, 'FaceAlpha', 1);
+        hold on; p = plot3(x,y,z, 'ko', 'MarkerFaceColor', 'black');
+        plot3(mics_locs(:, 1), mics_locs(:, 2), mics_locs(:, 3), 'sg')
+    else
+        h=trisurf(fMat,x,y,z, 'FaceColor', 'none', 'EdgeColor', 0.5*[1 1 1],'LineWidth', 1 );
+        hold on; p = plot3(x,y,z, 'ko', 'MarkerFaceColor', 'black');
+        plot3(mics_locs(:, 1), mics_locs(:, 2), mics_locs(:, 3), 'sg', 'MarkerFaceColor', 'green')
+    end
+    
+    hold on;plot3(test_point(:, 1), test_point(:, 2), test_point(:, 3), 'rp', 'MarkerFaceColor', 'red') 
+    
+    axis equal;
+    view(2)
+    
+    xlabel('x-axis (m)');
+    ylabel('y-axis (m)');
+    zlabel('z-axis (m)');
+    title( ['test points']);
+    
+    axis equal;
+    grid off;
+end
+%% finding tao(differential time delay) for test points
+
+tao_test = zeros(size(mics_locs, 1), size(mics_locs, 1), size(vMat, 1));
+
+for i = 1:size(mics_locs, 1)
+    for j = 1:size(mics_locs, 1)
+        tao_test(i, j, :) = (1/c)*dot(repmat(mics_locs(i, :) - mics_locs(j, :), size(vMat, 1), 1) , vMat, 2);
+    end
+end
+
+% first mic is reference
+steering_test = zeros(size(mics_locs, 1), size(vMat, 1));
+for i = 1:size(mics_locs, 1)
+    steering_test(i, :) = (1/c)*dot(repmat(mics_locs(1, :) - mics_locs(j, :), size(vMat, 1), 1) , vMat, 2);
+end
+
+%% test
+
+test_steer = zeros(size(mics_locs, 1), 1);
+for i = 1:size(mics_locs, 1)
+    test_steer(i) = (1/c)*dot(mics_locs(1, :) - mics_locs(i, :) , test_point, 2);
+end
+
+corr_test = dot(repmat(test_steer, 1, size(steering_test, 2)) , steering_test, 1);
+[~, ind_max] = max(corr_test);
+
+plot3(x(ind_max),y(ind_max),z(ind_max), 'bv', 'MarkerFaceColor', 'blue');
+
+
+
+
+
+
+
+
+
+
+
+
