dab379412e074c9cd2050d68ee70f8b74cb76fb1
amir hosein <a.h.keshavarz74@gmail.com>
2020-10-25 22:04:28 +0330

    moving target is added.
    - just AOA is being computed.
    PROBLEM: Kalman filter prototype must be added.

--- main.m
+++ main.m
@@ -11,12 +11,17 @@ c = 330; % m/s speed of sound
 
 L = 3;
 nPoints = 10*4^L + 2;
-test_points_NUM = 4;
+test_points_NUM = 2;
 
-test_point = rand(test_points_NUM, 3)-0.5;
-for j = 1:size(test_point, 1)
-    test_point(j, :) = test_point(j, :) / norm(test_point(j, :));
+N = 10;
+test_vel = 0.5*(rand(test_points_NUM, 3) - 0.5);
+test_point_initial = 2*(rand(test_points_NUM, 3)-0.5);
+test_point = zeros(test_points_NUM, 3, N);
+test_point(:, :, 1) = test_point_initial;
+for i = 2:N
+    test_point(:, :, i) = test_point(:, :, i-1) + test_vel;
 end
+
 %% microphones location
 theta = linspace(0, 2*pi, 9).';
 mics_locs = 0.254/2*[cos(theta), sin(theta), zeros(length(theta), 1)];
@@ -30,6 +35,7 @@ mics_locs = [mics_locs; mics_locs2];
 [vMat, fMat] = spheretri(nPoints);
 
 %% plot
+
 if icahedron_plot == true
     x = vMat(:, 1);
     y = vMat(:, 2);
@@ -44,11 +50,6 @@ if icahedron_plot == true
         plot3(mics_locs(:, 1), mics_locs(:, 2), mics_locs(:, 3), 'sg', 'MarkerFaceColor', 'green')
     end
     
-    hold on;plot3(test_point(:, 1), test_point(:, 2), test_point(:, 3), 'rp', 'MarkerFaceColor', 'red')
-    
-    axis equal;
-    view(2)
-    
     xlabel('x-axis (m)');
     ylabel('y-axis (m)');
     zlabel('z-axis (m)');
@@ -56,7 +57,9 @@ if icahedron_plot == true
     
     axis equal;
     grid off;
+    
 end
+
 %% finding tao(differential time delay) for test points
 
 tao_test = zeros(size(mics_locs, 1), size(mics_locs, 1), size(vMat, 1));
@@ -73,31 +76,57 @@ for i = 1:size(mics_locs, 1)
     steering_test(i, :) = (fs/c)*dot(repmat(mics_locs(1, :) - mics_locs(i, :), size(vMat, 1), 1) , vMat, 2);
 end
 
-%% test
-
-test_steer = zeros(size(mics_locs, 1), size(test_point, 1));
-
-% test_point = vMat(15, :);
-% hold on;plot3(test_point(:, 1), test_point(:, 2), test_point(:, 3), 'rp', 'MarkerFaceColor', 'red')
-for j = 1:size(test_point, 1)
-    for i = 1:size(mics_locs, 1)
-        test_steer(i, j) = (fs/c)*dot(mics_locs(1, :) - mics_locs(i, :) , test_point(j, :), 2);
+%%
+for iter = 1:N
+    test_point_norm = test_point(:, :, iter);
+    test_point_norm = test_point_norm / norm(test_point_norm);
+    
+    hold on;plot3(test_point(:, 1, iter), test_point(:, 2, iter), test_point(:, 3, iter), 'rp', 'MarkerFaceColor', 'red')
+    
+    test_steer = zeros(size(mics_locs, 1), size(test_point, 1));
+    
+    % test_point = vMat(15, :);
+    % hold on;plot3(test_point(:, 1), test_point(:, 2), test_point(:, 3), 'rp', 'MarkerFaceColor', 'red')
+    for j = 1:size(test_point, 1)
+        for i = 1:size(mics_locs, 1)
+            test_steer(i, j) = (fs/c)*dot(mics_locs(1, :) - mics_locs(i, :) , test_point_norm(j, :), 2);
+        end
     end
+    
+    for j = 1:size(test_point, 1)
+        corr_test = repmat(test_steer(:, j), 1, size(steering_test, 2)) - steering_test;
+        corr_test = sum(corr_test.^2).^0.5;
+        % corr_test = dot(repmat(test_steer, 1, size(steering_test, 2)) , steering_test, 1);
+        [~, ind_max(j)] = min(corr_test);
+    end
+    
+    estimated_locs = [x(ind_max),y(ind_max),z(ind_max)];
+    
+    plot3(estimated_locs(:, 1), estimated_locs(:, 2), estimated_locs(:, 3), 'bv', 'MarkerFaceColor', 'blue')
+    
+end
+%% tracking
+
+% parameters
+delta_T = 0.5;
+F = eye(6);
+for i = 4:6
+    F(i-3, i) = delta_T;
 end
 
-for j = 1:size(test_point, 1)
-    corr_test = repmat(test_steer(:, j), 1, size(steering_test, 2)) - steering_test;
-    corr_test = sum(corr_test.^2).^0.5;
-    % corr_test = dot(repmat(test_steer, 1, size(steering_test, 2)) , steering_test, 1);
-    [~, ind_max(j)] = min(corr_test);
+sigma2_Q = 0.5;
+Q = zeros(6);
+for i = 4:6
+    Q(i, i) =  sigma2_Q;
 end
 
-plot3(x(ind_max),y(ind_max),z(ind_max), 'bv', 'MarkerFaceColor', 'blue');
+H = zeros(3, 6);
+for i = 1:3
+    H(i, i) = 1;
+end
 
-
-
-
-
+sigma2_R = 0.5;
+R = sigma2_R*eye(3);
 
 
 
